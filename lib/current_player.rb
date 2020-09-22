0552d20e3807936363afedd25015becb3b031212
elazar cohen <escohen115@gmail.com>
2020-09-22 18:20:08 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+  turns = 0
+  board.each do |spaces|
+    if space == "X" || "O"
+      turns += 1
+    end
+  end
+  turns
+end
+
+
+def current_player(board)
+  if turn_count % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
